4af0042f1e31fd91421418af4f430ae3a8adfd5b
Anthony Bazilio <anthonybazilio@gmail.com>
2018-09-08 14:11:44 -0300

    start

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+
+app.set('view engine', 'njk');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.post('/auth', (req, res) => {
+    res.send('Você logou com o usuário ${req.body.username} e senha ${req.body.password}');
+});
+
+app.listen(3000);
